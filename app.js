7affe6d582f03a4bc9c4487f308fa5c8b7c45694
Sarahhabbi <habbisarah.1@gmail.com>
2021-08-25 19:11:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// // animation header
+// window.addEventListener("scroll", ()=>{
+//     document.querySelector("header").classList.add("nav-scrolled",window.scrollY>0);
+// })
+
+
+// // GOOGLE MAPS contact.html
+// function initMap(){
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 48.71555563867689, lng: 2.248638069317734},
+//         zoom: 15,
+//         mapId: '56f4ea2ad69f76e'
+//       });
+
+//     const marker = new google.maps.Marker({
+//         position: {lat: 48.71555563867689, lng: 2.248638069317734},
+//         map: map,
+//       });
+      
+//     const infowindow = new google.maps.InfoWindow({
+//         content: "PALAISEAU PIECES AUTO",
+//     });
+    
+//     marker.addListener("mouseover", () => {
+//         infowindow.open(map,marker);
+//     });
+    
+// }
+
+
+// jQuery(document).ready(function( $ ) {
+//     $("#google-reviews").googlePlaces({
+//          placeId: 'ChIJww7iohR55kcRC1g-TFrZcPw'
+//        , render: ['reviews']
+//        , min_rating: 4
+//        , max_rows:4
+//     });
+//  });
+
+// ChIJww7iohR55kcRC1g-TFrZcPw  ID
+// AIzaSyAEkILp6nBCMmes5oS0KupQpav2SSnxYWY  API KEY
+
