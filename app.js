296ab5c481f121912f82f879620f4f50ff7c24e3
Sarahhabbi <habbisarah.1@gmail.com>
2021-08-28 20:20:45 +0200

    update

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-// // animation header
-// window.addEventListener("scroll", ()=>{
-//     document.querySelector("header").classList.add("nav-scrolled",window.scrollY>0);
-// })
+// ANIMATION NAV BAR
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY>0){
+        document.querySelector("header").classList.add("nav-scrolled",window.scrollY>0);
+    }
+    else{
+        document.querySelector("header").classList.remove("nav-scrolled",window.scrollY>0);
+    }
+})
+
+// display hamburger menu
+
+const nav = document.querySelector("nav");
+const hamburger = document.querySelector(".bi-list");
+const logo = document.querySelector(".logo");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("bi-x-lg");
+    nav.classList.toggle("open");
+    logo.classList.toggle("logo-hidden");
+})
+
+
+
+jQuery(document).ready(function( $ ) {
+    $("#google-reviews").googlePlaces({
+         placeId: 'ChIJww7iohR55kcRC1g-TFrZcPw'
+       , render: ['reviews']
+       , min_rating: 4
+       , max_rows:4
+    });
+ });
+
+// SLICK SLIDER
+$(document).ready(function(){
+    $('.google-reviews').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
 
 
 // // GOOGLE MAPS contact.html
@@ -26,17 +63,3 @@
 //     });
     
 // }
-
-
-// jQuery(document).ready(function( $ ) {
-//     $("#google-reviews").googlePlaces({
-//          placeId: 'ChIJww7iohR55kcRC1g-TFrZcPw'
-//        , render: ['reviews']
-//        , min_rating: 4
-//        , max_rows:4
-//     });
-//  });
-
-// ChIJww7iohR55kcRC1g-TFrZcPw  ID
-// AIzaSyAEkILp6nBCMmes5oS0KupQpav2SSnxYWY  API KEY
-
